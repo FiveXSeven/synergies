894b5c936dd7c4c8e695d7acbfca9d5af9c08212
FiveXSeven <nazairesalomon333@gmail.com>
2025-05-15 14:53:29 +0000

    technique done

--- app/components/about/about.component.ts
+++ app/components/about/about.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
