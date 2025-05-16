1bbb8ac2162263a1704a0dd4ed52329cf2b3ffee
FiveXSeven <nazairesalomon333@gmail.com>
2025-05-16 13:20:53 +0000

    setup firebase

--- app/app.config.ts
+++ app/app.config.ts
@@ -2,10 +2,13 @@ import {
     ApplicationConfig,
     provideZoneChangeDetection,
     LOCALE_ID,
+    EnvironmentProviders,
 } from "@angular/core";
 import { provideRouter, withInMemoryScrolling } from "@angular/router";
-
 import { routes } from "./app.routes";
+import { initializeApp, provideFirebaseApp } from "@angular/fire/app";
+import { getAuth, provideAuth } from "@angular/fire/auth";
+import { environment } from "../environments/environment";
 
 export const appConfig: ApplicationConfig = {
     providers: [
@@ -19,5 +22,19 @@ export const appConfig: ApplicationConfig = {
             })
         ),
         { provide: LOCALE_ID, useValue: "fr-FR" },
+
+        importProvidersFrom([
+            provideFirebaseApp(() => initializeApp(environment.firebase)),
+            provideAuth(() => getAuth()),
+            // provideFirestore(() => getFirestore()),
+            // provideStorage(() => getStorage()),
+        ]),
     ],
 };
+function importProvidersFrom(
+    arg0: EnvironmentProviders[]
+):
+    | import("@angular/core").Provider
+    | import("@angular/core").EnvironmentProviders {
+    throw new Error("Function not implemented.");
+}
